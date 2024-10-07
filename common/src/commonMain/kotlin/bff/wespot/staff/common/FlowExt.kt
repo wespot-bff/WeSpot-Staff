ed7ea3e103af136f73f4d1835ca0cd81a4b43a5a
jeongjaino <akgk155@gmail.com>
2024-10-07 18:39:22 +0900

    [FEAT] : Common Flow, Time Extenstions 구현

---
+++ common/src/commonMain/kotlin/bff/wespot/staff/common/FlowExt.kt
@@ -0,0 +1,25 @@
+package bff.wespot.staff.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+fun <T> Flow<T>.collectEvent(
+    lifecycleState: Lifecycle.State = Lifecycle.State.STARTED,
+    event: (suspend (sideEffect: T) -> Unit)
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val callback by rememberUpdatedState(newValue = event)
+
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(lifecycleState) {
+            this@collectEvent.collect { callback(it) }
+        }
+    }
+}
