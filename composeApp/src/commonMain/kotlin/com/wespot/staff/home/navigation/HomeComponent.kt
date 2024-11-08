a8b969f1d2ece51dd8f9d745eadd8de5403e5aa8
jeongjaino <akgk155@gmail.com>
2024-11-08 19:32:27 +0900

    [FEATURE]#1 : Home Component 구현

---
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/home/navigation/HomeComponent.kt
@@ -0,0 +1,7 @@
+package com.wespot.staff.home.navigation
+
+import com.arkivanov.decompose.ComponentContext
+
+class HomeComponent(
+    componentContext: ComponentContext,
+): ComponentContext by componentContext
