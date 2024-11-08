f2109ed8a4675cb501e2a3d3c5e86edf8b688456
jeongjaino <akgk155@gmail.com>
2024-11-08 19:32:37 +0900

    [FEATURE]#1 : Vote Component 구현

---
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/vote/navigation/VoteComponent.kt
@@ -0,0 +1,7 @@
+package com.wespot.staff.vote.navigation
+
+import com.arkivanov.decompose.ComponentContext
+
+class VoteComponent(
+    componentContext: ComponentContext,
+): ComponentContext by componentContext
