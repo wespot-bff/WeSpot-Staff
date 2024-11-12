d953360e7ef35c37e908bc333b17cfc5282751f9
jeongjaino <akgk155@gmail.com>
2024-11-13 01:27:59 +0900

    [CHORE]#4 : VoteScreen -> VoteHomeScreen

--- composeApp/src/commonMain/kotlin/com/wespot/staff/WeSpotStaffApp.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/WeSpotStaffApp.kt
@@ -1,5 +1,10 @@
 package com.wespot.staff
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,49 +15,69 @@ import com.arkivanov.decompose.extensions.compose.stack.Children
 import com.arkivanov.decompose.extensions.compose.stack.animation.fade
 import com.arkivanov.decompose.extensions.compose.stack.animation.stackAnimation
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.router.stack.active
 import com.wespot.staff.designsystem.theme.WeSpotTheme
 import com.wespot.staff.message.MessageScreen
 import com.wespot.staff.navigation.RootComponent
 import com.wespot.staff.navigation.RootComponent.RootChild
 import com.wespot.staff.report.ReportScreen
-import com.wespot.staff.vote.VoteScreen
-import com.wespot.staff.vote.question.QuestionScreen
+import com.wespot.staff.vote.navigation.VoteNavigation
+import com.wespot.staff.vote.navigation.VoteRootComponent
 
 @Composable
 fun WeSpotStaffApp(component: RootComponent) {
-    WeSpotTheme {
-        val currentSelectedItem = remember { mutableStateOf(component.initialConfiguration) }
+    val childStack by component.stack.subscribeAsState()
+    val child = childStack.active.instance
 
+    WeSpotTheme {
         Scaffold(
             bottomBar = {
-                BottomNavigationTab(
-                    selectedNavigation = currentSelectedItem.value,
-                    onNavigationSelected = { selected ->
-                        currentSelectedItem.value = selected
-                        component.navigateRoot(selected)
+                AnimatedContent(
+                    targetState = isBottomBannerVisible(child),
+                    transitionSpec = {
+                        fadeIn(animationSpec = tween()) togetherWith fadeOut(animationSpec = tween())
                     },
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            },
+                    label = "Bottom Navigation Bar"
+                ) {
+                    val currentSelectedItem = remember { mutableStateOf(component.initialConfiguration) }
+                    BottomNavigationTab(
+                        selectedNavigation = currentSelectedItem.value,
+                        onNavigationSelected = { selected ->
+                            currentSelectedItem.value = selected
+                            component.navigateRoot(selected)
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
+            }
         ) {
             Box(modifier = Modifier.padding(it)) {
-                AppNavigation(component)
+                AppNavigation(childStack)
             }
         }
     }
 }
 
 @Composable
-fun AppNavigation(component: RootComponent) {
-    val childStack by component.stack.subscribeAsState()
+fun AppNavigation(childStack: ChildStack<*, RootChild>) {
     Children(
         stack = childStack,
         animation = stackAnimation(fade()),
     ) {
         when (val child = it.instance) {
-            is RootChild.VoteRoot -> QuestionScreen(component = child.component)
+            is RootChild.VoteRoot -> VoteNavigation(child.component)
             is RootChild.MessageRoot -> MessageScreen(component = child.component)
             is RootChild.ReportRoot -> ReportScreen(component = child.component)
         }
     }
 }
+
+fun isBottomBannerVisible(child: RootChild): Boolean =
+    when (child) {
+        is RootChild.VoteRoot -> {
+            val voteChild = child.component.stack.active.instance
+            voteChild !is VoteRootComponent.VoteChild.QuestionScreen
+        }
+        is RootChild.MessageRoot, is RootChild.ReportRoot -> true
+    }
