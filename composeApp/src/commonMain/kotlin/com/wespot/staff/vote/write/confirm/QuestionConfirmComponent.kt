6f810220ee74e45e932a7800f0f4af555e9288d6
jeongjaino <akgk155@gmail.com>
2024-11-16 00:27:17 +0900

    [FEATURE]#4 : 파싱 후 질문지 변환 확인 화면 구현

--- composeApp/src/commonMain/kotlin/com/wespot/staff/vote/write/confirm/QuestionConfirmComponent.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/vote/write/confirm/QuestionConfirmComponent.kt
@@ -1,4 +1,15 @@
 package com.wespot.staff.vote.write.confirm
 
-class QuestionConfirmComponent {
+import com.arkivanov.decompose.ComponentContext
+import com.wespot.staff.domain.vote.VoteQuestionContent
+
+class QuestionConfirmComponent(
+    componentContext: ComponentContext,
+    val questionList: List<VoteQuestionContent>,
+    val popBackStack: () -> Unit,
+    val navigateToQuestion: (String) -> Unit,
+): ComponentContext by componentContext {
+    fun navigateUp() = popBackStack()
+
+    fun navigateToQuestionScreen(message: String) = navigateToQuestion(message)
 }
--- composeApp/src/commonMain/kotlin/com/wespot/staff/vote/write/confirm/QuestionConfirmScreen.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/vote/write/confirm/QuestionConfirmScreen.kt
@@ -1,24 +1,59 @@
 package com.wespot.staff.vote.write.confirm
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.wespot.staff.designsystem.component.WSTopBar
+import com.wespot.staff.designsystem.theme.StaticTypography
+import com.wespot.staff.designsystem.theme.WeSpotThemeManager
+import com.wespot.staff.vote.write.add.QuestionAddScreen
+import com.wespot.staff.vote.write.add.QuestionAddViewModel
+import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuestionConfirmScreen(
     component: QuestionConfirmComponent,
+    questionAddViewModel: QuestionAddViewModel = koinViewModel(),
 ) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             WSTopBar(
                 title = "",
                 canNavigateBack = true,
-                navigateUp = { },
+                navigateUp = component::navigateUp,
             )
         }
     ) { innerPadding ->
-
+        Box(modifier = Modifier.padding(innerPadding)) {
+            QuestionAddScreen(
+                viewModel = questionAddViewModel,
+                questionList = component.questionList,
+                titleContent = {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 16.dp),
+                        text = "변환된 질문 확인하기",
+                        style = StaticTypography().header1,
+                        color = WeSpotThemeManager.colors.txtTitleColor,
+                    )
+                },
+                showToast = { snackbarHostState.showSnackbar(it) },
+                navigateToQuestionScreen = component::navigateToQuestionScreen
+            )
+        }
     }
 }
