5ddbab093ab35025dec93d52b28cefa9a5f2875c
jeongjaino <akgk155@gmail.com>
2024-11-16 00:30:02 +0900

    [CHORE]#4 : QuestionParseViewModel 삭제

--- composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
@@ -2,13 +2,11 @@ package com.wespot.staff.di
 
 import com.wespot.staff.vote.question.QuestionViewModel
 import com.wespot.staff.vote.write.add.QuestionAddViewModel
-import com.wespot.staff.vote.write.parse.QuestionParseViewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule: Module = module {
     viewModelOf(::QuestionViewModel)
-    viewModelOf(::QuestionParseViewModel)
     viewModelOf(::QuestionAddViewModel)
 }
