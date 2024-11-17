29a7d8e0733c87925cb3d95b3ae00a45690c5458
jeongjaino <akgk155@gmail.com>
2024-11-17 15:11:40 +0900

    [FIX]#6 : koin 초기화 로직 iosMain -> iosApp으로 변경

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,9 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+    init() { AppModuleKt.doInitKoin() }
     var body: some Scene {
         WindowGroup {
             ContentView()
