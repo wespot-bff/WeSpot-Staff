0e1334625b9a737a80f9eddd916e9bab0cd01f4d
jeongjaino <akgk155@gmail.com>
2024-11-20 23:46:18 +0900

    [FEATURE]#8 : Android/iOS Firebase 초기화 로직 추가

--- composeApp/src/androidMain/kotlin/com/wespot/staff/WeSpotStaffApplication.kt
+++ composeApp/src/androidMain/kotlin/com/wespot/staff/WeSpotStaffApplication.kt
@@ -1,14 +1,19 @@
 package com.wespot.staff
 
 import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import com.wespot.staff.di.initKoin
 import org.koin.android.ext.koin.androidContext
 
 class WeSpotStaffApplication: Application() {
     override fun onCreate() {
         super.onCreate()
-        initKoin {
-            androidContext(this@WeSpotStaffApplication)
-        }
+        initKoin { androidContext(this@WeSpotStaffApplication) }
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        Firebase.initialize(this)
     }
 }
