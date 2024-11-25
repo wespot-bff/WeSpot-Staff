87c8c310dd5ec5677cdd9fa73d5d9d76f1ece6b6
jeongjaino <akgk155@gmail.com>
2024-11-25 22:58:52 +0900

    [FEATURE]#8 : viewModel 의존성 주입 구현

--- composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
@@ -2,6 +2,9 @@ package com.wespot.staff.di
 
 import com.wespot.staff.vote.question.QuestionViewModel
 import com.wespot.staff.vote.write.add.QuestionAddViewModel
+import com.wespot.staff.RootViewModel
+import com.wespot.staff.entire.configuration.ConfigurationViewModel
+import com.wespot.staff.entire.notification.NotificationViewModel
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
@@ -9,4 +12,7 @@ import org.koin.dsl.module
 val viewModelModule: Module = module {
     viewModelOf(::QuestionViewModel)
     viewModelOf(::QuestionAddViewModel)
+    viewModelOf(::ConfigurationViewModel)
+    viewModelOf(::NotificationViewModel)
+    viewModelOf(::RootViewModel)
 }
