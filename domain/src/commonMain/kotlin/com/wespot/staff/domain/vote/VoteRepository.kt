a0b848f3f5177e5b1880a1ffe0abfe9d9836f988
jeongjaino <akgk155@gmail.com>
2024-11-25 22:57:01 +0900

    [REFACTOR]#8 : 투표 질문 작성 화면 로직 수정

--- domain/src/commonMain/kotlin/com/wespot/staff/domain/vote/VoteRepository.kt
+++ domain/src/commonMain/kotlin/com/wespot/staff/domain/vote/VoteRepository.kt
@@ -1,10 +1,8 @@
 package com.wespot.staff.domain.vote
 
-import kotlinx.coroutines.flow.Flow
-
 interface VoteRepository {
-    fun getVoteQuestionsStream(): Flow<List<VoteQuestion>>
     suspend fun postVoteQuestion(question: VoteQuestionContent): Result<Unit>
     suspend fun editVoteQuestion(id: Long, question: VoteQuestionContent): Result<Unit>
     suspend fun postVoteQuestions(questions: List<VoteQuestionContent>): Result<Unit>
+    suspend fun getVoteQuestions(): Result<List<VoteQuestion>>
 }
