52e0b3522333bb29a9430a8553e371c997be3ec9
jeongjaino <akgk155@gmail.com>
2024-11-25 22:45:07 +0900

    [CHORE]#8 : 신고에서 전체 탭으로 변경

--- composeApp/src/commonMain/kotlin/com/wespot/staff/report/navigation/ReportComponent.kt
+++
@@ -1,7 +0,0 @@
-package com.wespot.staff.report.navigation
-
-import com.arkivanov.decompose.ComponentContext
-
-class ReportComponent(
-    componentContext: ComponentContext,
-): ComponentContext by componentContext
