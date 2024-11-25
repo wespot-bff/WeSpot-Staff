c0bda471c76d6cfb8d94cb14bc76f1bf6707c509
jeongjaino <akgk155@gmail.com>
2024-11-25 22:54:48 +0900

    [FEATURE]#8 : 소스 및 레포지토리 의존성 주입 설정

--- data/src/iosMain/kotlin/com/wespot/staff/data/di/DataModule.kt
+++ data/src/iosMain/kotlin/com/wespot/staff/data/di/DataModule.kt
@@ -1,13 +1,24 @@
 package com.wespot.staff.data.di
 
+import com.wespot.staff.data.config.DefaultRemoteConfigRepository
+import com.wespot.staff.data.config.remote.RemoteConfigDataSource
+import com.wespot.staff.data.config.remote.DefaultRemoteConfigDataSource
 import com.wespot.staff.data.core.createDataStore
 import com.wespot.staff.data.core.defaultKtorConfig
+import com.wespot.staff.data.notification.NotificationApiClient
+import com.wespot.staff.data.notification.DefaultNotificationApiClient
+import com.wespot.staff.data.notification.DefaultNotificationRepository
 import com.wespot.staff.data.vote.DefaultVoteRepository
 import com.wespot.staff.data.vote.local.DefaultVoteDataStore
 import com.wespot.staff.data.vote.local.VoteDataStore
 import com.wespot.staff.data.vote.remote.DefaultVoteApiClient
 import com.wespot.staff.data.vote.remote.VoteApiClient
+import com.wespot.staff.domain.config.RemoteConfigRepository
+import com.wespot.staff.domain.notification.NotificationRepository
 import com.wespot.staff.domain.vote.VoteRepository
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.remoteconfig.FirebaseRemoteConfig
+import dev.gitlive.firebase.remoteconfig.remoteConfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.darwin.Darwin
 import org.koin.core.module.Module
@@ -16,16 +27,24 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 public actual val dataModule: Module = module {
+    // Remote
     single {
         HttpClient(Darwin) {
             defaultKtorConfig()
         }
     }
 
+    // Local
     single<VoteDataStore> {
         DefaultVoteDataStore(createDataStore())
     }
 
+    // Source
+    single<FirebaseRemoteConfig> {
+        Firebase.remoteConfig
+    }
+
+    // Repository
     single<Repositories> {
         DefaultRepositories(
             mapOf(
@@ -34,6 +53,11 @@ public actual val dataModule: Module = module {
         )
     }
 
+    // Binds
     singleOf(::DefaultVoteApiClient) bind VoteApiClient::class
     singleOf(::DefaultVoteRepository) bind VoteRepository::class
+    singleOf(::DefaultRemoteConfigDataSource) bind RemoteConfigDataSource::class
+    singleOf(::DefaultRemoteConfigRepository) bind RemoteConfigRepository::class
+    singleOf(::DefaultNotificationApiClient) bind NotificationApiClient::class
+    singleOf(::DefaultNotificationRepository) bind NotificationRepository::class
 }
