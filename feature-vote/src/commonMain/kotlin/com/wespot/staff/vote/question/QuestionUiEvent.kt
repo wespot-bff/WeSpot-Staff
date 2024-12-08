dd13aae41ff74406961e08841d3eb3e9bc1b9699
jeongjaino <akgk155@gmail.com>
2024-12-08 14:43:25 +0900

    [FEATURE]#13 : Vote Domain BaseViewModel 적용

--- feature-vote/src/commonMain/kotlin/com/wespot/staff/vote/question/QuestionUiEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.wespot.staff.vote.question
-
-sealed class QuestionUiEvent {
-    data object QuestionLoadFailedEvent : QuestionUiEvent()
-    data class QuestionPostEvent(val message: String) : QuestionUiEvent()
-}
--- feature-vote/src/commonMain/kotlin/com/wespot/staff/vote/write/add/QuestionAddUiEvent.kt
+++
@@ -1,6 +0,0 @@
-package com.wespot.staff.vote.write.add
-
-sealed interface QuestionAddUiEvent {
-    data object NavigateToQuestionScreen : QuestionAddUiEvent
-    data class ShowToast(val message: String): QuestionAddUiEvent
-}
