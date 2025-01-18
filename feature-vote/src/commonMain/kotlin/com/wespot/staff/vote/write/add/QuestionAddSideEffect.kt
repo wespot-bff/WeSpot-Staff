d558f2c886b50466247d8bc12a6ea00d1409e556
jeongjaino <akgk155@gmail.com>
2025-01-18 17:57:09 +0900

    [REFACTOR]#22 : Navigate, ShowSnackbar SideEffect 분리

--- feature-vote/src/commonMain/kotlin/com/wespot/staff/vote/write/add/QuestionAddSideEffect.kt
+++ feature-vote/src/commonMain/kotlin/com/wespot/staff/vote/write/add/QuestionAddSideEffect.kt
@@ -2,5 +2,5 @@ package com.wespot.staff.vote.write.add
 
 sealed interface QuestionAddSideEffect {
     data object NavigateToQuestionScreen : QuestionAddSideEffect
-    data class ShowToast(val message: String): QuestionAddSideEffect
+    data class ShowSnackbar(val message: String): QuestionAddSideEffect
 }
