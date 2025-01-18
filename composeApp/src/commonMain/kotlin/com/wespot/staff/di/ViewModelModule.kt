ef9a4d4519ba987992341cabd60fa898fe6c5fdd
jeongjaino <akgk155@gmail.com>
2025-01-18 17:42:54 +0900

    [CHORE]#22 : RootViewModel 패키지 path 변경

--- composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
+++ composeApp/src/commonMain/kotlin/com/wespot/staff/di/ViewModelModule.kt
@@ -2,7 +2,7 @@ package com.wespot.staff.di
 
 import com.wespot.staff.vote.question.QuestionViewModel
 import com.wespot.staff.vote.write.add.QuestionAddViewModel
-import com.wespot.staff.RootViewModel
+import com.wespot.staff.state.RootViewModel
 import com.wespot.staff.entire.configuration.ConfigurationViewModel
 import com.wespot.staff.entire.configuration.add.ConfigurationAddViewModel
 import com.wespot.staff.entire.notification.NotificationViewModel
