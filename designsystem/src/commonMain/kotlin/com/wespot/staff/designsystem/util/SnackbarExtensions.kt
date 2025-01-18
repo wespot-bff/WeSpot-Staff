8ee834ab546fce71f3cbae74daf2fc33035ad13b
jeongjaino <akgk155@gmail.com>
2025-01-18 17:37:46 +0900

    [FEATURE]#22 : SnackbarHost 생성 및 CompostionLocal 구현

--- designsystem/src/commonMain/kotlin/com/wespot/staff/designsystem/util/SnackbarExtensions.kt
+++
@@ -1,8 +0,0 @@
-package com.wespot.staff.designsystem.util
-
-import androidx.compose.material.SnackbarHostState
-import androidx.compose.runtime.compositionLocalOf
-
-val LocalSnackbarHostState = compositionLocalOf<SnackbarHostState>{
-    error("No Snackbar Host State")
-}
---
+++ designsystem/src/commonMain/kotlin/com/wespot/staff/designsystem/util/snackbar/SnackbarHost.kt
@@ -0,0 +1,5 @@
+package com.wespot.staff.designsystem.util.snackbar
+
+interface SnackbarHost {
+    fun showSnackbar(message: String)
+}
